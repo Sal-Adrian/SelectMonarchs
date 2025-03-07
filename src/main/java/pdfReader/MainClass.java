2b5bdb22e98c1da3c30f42a94280e83be058ed2c
A-Sal3 <asalinas3205@gmail.com>
2025-03-07 17:37:16 -0600

    Program can read txt files.

--- src/main/java/pdfReader/MainClass.java
+++ src/main/java/pdfReader/MainClass.java
@@ -1,6 +1,8 @@
 package pdfReader;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -11,17 +13,36 @@ import org.apache.pdfbox.text.PDFTextStripper;
 public class MainClass {
 
 	public static void main(String[] args) throws IOException {
-		File pdfFile = new File(".\\Dictionary_of_Proverbs.pdf");
-		PDDocument document = Loader.loadPDF(pdfFile);
-		PDFTextStripper pdfStripper = new PDFTextStripper();
-		String text = pdfStripper.getText(document);
-		document.close();
+        boolean txtReaderMode = true;
+        if (txtReaderMode) {
+            txtReader();
+        } else {
+            File pdfFile = new File(".\\Dictionary_of_Proverbs.pdf");
+            PDDocument document = Loader.loadPDF(pdfFile);
+            PDFTextStripper pdfStripper = new PDFTextStripper();
+            String text = pdfStripper.getText(document);
+            document.close();
 
-        PrintWriter writer = new PrintWriter("sayings.txt", "UTF-8");
-        writer.println(text);
-        writer.close();
-		
-		// System.out.println(text);
+            PrintWriter writer = new PrintWriter("sayings.txt", "UTF-8");
+            writer.println(text);
+            writer.close();
+        }
 	}
+
+    static void txtReader() {
+        String fileName = "sayings/african.txt";
+        try(BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+            String everything = sb.toString();
+            System.out.println(everything);
+        } catch(IOException e) { }
+    }
 }
 
