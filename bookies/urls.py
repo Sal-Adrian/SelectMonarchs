8298249b29ed32258a0c3f859a5d456af20635ce
A-Sal3 <asalinas3205@gmail.com>
2025-06-26 10:53:03 -0500

    Created URLs

--- bookies/urls.py
+++ bookies/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:bet_id>/", views.detail, name="detail"),
+    path("<int:bet_id>/results/", views.results, name="results"),
+    path("<int:bet_id>/vote/", views.vote, name="vote"),
 ]
--- bookies/views.py
+++ bookies/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
+from .models import Bet
 
 def index(request):
-    return HttpResponse("You're at the bookies index.")
+    latest_bet_list = Bet.objects.order_by("-pub_date")[:5]
+    output = ", ".join([b.bet_text for b in latest_bet_list])
+    return HttpResponse(output)
+
+
+def detail(request, bet_id):
+    return HttpResponse("You're looking at bet %s." % bet_id)
+
+
+def results(request, bet_id):
+    response = "You're looking at the results of bet %s."
+    return HttpResponse(response % bet_id)
+
+
+def bet(request, bet_id):
+    return HttpResponse("You're betting on bet %s." % bet_id)
