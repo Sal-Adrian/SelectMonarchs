568ceb92c01ce5d406583b978bb9b00764dac284
A-Sal3 <asalinas3205@gmail.com>
2025-06-27 15:54:57 -0500

    Formating improvements

--- bookies/admin.py
+++ bookies/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 
 from .models import Choice, Bet
 
-admin.site.register(Bet)
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+
+class BetAdmin(admin.ModelAdmin):
+    list_display = ["bet_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    fieldsets = [
+        (None, {"fields": ["bet_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+    search_fields = ["bet_text"]
+
+admin.site.register(Bet, BetAdmin)
