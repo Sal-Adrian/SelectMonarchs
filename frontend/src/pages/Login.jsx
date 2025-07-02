3da9758d0ddd956e9bb2369edaa6fe27d6ee7ada
A-Sal3 <asalinas3205@gmail.com>
2025-07-02 16:56:55 -0500

    Link to and from Login and Register page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,11 @@
 import Form from '../components/Form'
+import { Link } from 'react-router-dom';
 
 function Login() {
     return (
         <div>
             <Form route="/api/token/" method="login" />
+            <p>Don't have an account? <Link to="/register">Register</Link></p>
         </div>
     )
 }
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,11 @@
 import Form from '../components/Form'
+import { Link } from 'react-router-dom';
 
 function Register() {
     return (
         <div>
             <Form route="/api/user/register/" method="register" />
+            <p>Already have an account? <Link to="/login">Login</Link></p>
         </div>
     )
 }
