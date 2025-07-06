73386accad9b79a72d882e4241eaf5de3c420b54
A-Sal3 <asalinas3205@gmail.com>
2025-07-06 12:03:06 -0500

    CSS changes

--- frontend/src/components/Bet.jsx
+++ frontend/src/components/Bet.jsx
@@ -1,18 +1,32 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function Bet({ bet }) {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <div key={bet.id}>
-            <li><Link to={`/${bet.id}`}>{bet.bet_text}</Link></li>
-            <ul>
-                {bet.choices.map((choice) => (
-                    <li key={choice.id}>{choice.choice_text} ==
-                    {choice.win_condition ? 
-                        Math.ceil(bet.win_probability*100) 
-                        : Math.ceil((1-bet.win_probability)*100)}%
-                    </li>
-                ))}
-            </ul>
+            <label>
+                <input className='absolute scale-0' type='checkbox' onClick={(e) => { setIsOpen(!isOpen); console.log(isOpen) }} />
+                <span className='justify-center'>
+                    <div className='flex justify-center border-2 border-blue-500'>
+                        <div className='font-bold border-2 border-blue-500'>
+                            <Link to={`/${bet.id}`}>{bet.bet_text}</Link>
+                        </div>
+                    </div>
+                    {bet.choices.map((choice) => (
+                        <p key={choice.id} className='mb-2 border-2 border-green-500'>
+                            {choice.choice_text} ==
+                            {choice.win_condition ?
+                                Math.ceil(bet.win_probability * 100)
+                                : Math.ceil((1 - bet.win_probability) * 100)}%
+                        </p>
+                    ))}
+                </span>
+            </label>
+
+
+
         </div>
     )
 }
