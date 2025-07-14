851d31639d5de0c4e55ec84f341ef6ae2e06c30d
A-Sal3 <asalinas3205@gmail.com>
2025-07-14 11:22:53 -0500

    Updated frontend visuals for register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -3,9 +3,14 @@ import { Link } from 'react-router-dom';
 
 function Register() {
     return (
-        <div>
+        <div className="min-h-screen flex flex-col items-center justify-center bg-black">
             <Form route="/api/user/register/" method="register" />
-            <p>Already have an account? <Link to="/login">Login</Link></p>
+            <p className="min-h-auto mt-6 text-white font-semibold text-center">
+                Already have an account?{' '}
+                <Link to="/login" className="text-green-400 font-extrabold underline hover:text-green-500 transition-colors">
+                    Login
+                </Link>
+            </p>
         </div>
     )
 }
