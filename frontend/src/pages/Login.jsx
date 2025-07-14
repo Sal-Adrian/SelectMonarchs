855d8fa7d4682b42136a06203c2d5475a87119ef
A-Sal3 <asalinas3205@gmail.com>
2025-07-14 11:22:18 -0500

    Updated frontend visuals for login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,9 +3,14 @@ import { Link } from 'react-router-dom';
 
 function Login() {
     return (
-        <div>
+        <div className="min-h-screen flex flex-col items-center justify-center bg-black">
             <Form route="/api/token/" method="login" />
-            <p>Don't have an account? <Link to="/register">Register</Link></p>
+            <p className="min-h-auto mt-6 text-white font-semibold text-center">
+                Don't have an account?{' '}
+                <Link to="/register" className="text-green-400 font-extrabold underline hover:text-green-500 transition-colors">
+                    Register
+                </Link>
+            </p>
         </div>
     )
 }
