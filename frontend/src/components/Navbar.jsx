418d40c5276840bf738db5fad3647ef344e16702
A-Sal3 <asalinas3205@gmail.com>
2025-07-17 10:58:40 -0500

    Consistant formatting

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import api from '../api';
 
 function Navbar() {
     const [user, setUser] = useState(null);
-    const username = localStorage.getItem("username");
+    const username = localStorage.getItem('username');
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -32,20 +32,20 @@ function Navbar() {
     };
 
     return (
-        <nav className='bg-gray-950 border-b border-stone-900 p-4'>
+        <nav className='sticky top-0 z-50 bg-gray-950 p-4'>
             {user && (
-                <div className="flex flex-col md:flex-row justify-between items-center">
-                    <span className="text-white text-2xl font-extrabold tracking-wide">
-                        Select<span className="text-green-500">Monarchs</span>
+                <div className='flex flex-col md:flex-row justify-between items-center'>
+                    <span className='text-white text-2xl font-extrabold tracking-wide'>
+                        Select<span className='text-green-500'>Monarchs</span>
                     </span>
-                    <div className="flex flex-col md:flex-row items-center mt-2 md:mt-0">
-                        <button className="text-white mr-0 md:mr-1 mb-2 md:mb-0 bg-stone-900 hover:bg-green-950 transition-colors duration-200 px-3 py-1 rounded-full border border-green-500 cursor-pointer" onClick={handleDeposit}>
+                    <div className='flex flex-col md:flex-row items-center mt-2 md:mt-0'>
+                        <button className='text-white mr-0 md:mr-1 mb-2 md:mb-0 bg-stone-900 hover:bg-green-950 transition-colors duration-200 px-3 py-1 rounded-full border border-green-500 cursor-pointer' onClick={handleDeposit}>
                             +$1000
                         </button>
-                        <span className="text-white font-semibold mr-0 md:mr-6 mb-2 md:mb-0 bg-stone-900 px-3 py-1 rounded-full border border-green-500">
+                        <span className='text-white font-semibold mr-0 md:mr-6 mb-2 md:mb-0 bg-stone-900 px-3 py-1 rounded-full border border-green-500'>
                             Balance: ${user.balance}
                         </span>
-                        <button className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-6 rounded-full shadow-md transition cursor-pointer" onClick={handleLogout}>
+                        <button className='bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-6 rounded-full shadow-md transition cursor-pointer' onClick={handleLogout}>
                             Logout
                         </button>
                     </div>
