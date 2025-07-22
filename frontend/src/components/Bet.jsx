bdb126586c687c855528f63d4dc239bd3d9b6985
A-Sal3 <asalinas3205@gmail.com>
2025-07-22 11:13:18 -0500

    Sliding overlay over bet text

--- frontend/src/components/Bet.jsx
+++ frontend/src/components/Bet.jsx
@@ -9,30 +9,17 @@ function Bet({ bet, banner }) {
             <Link
                 to={`/${bet.id}`}
                 onClick={(e) => e.stopPropagation()}
-                className='h-full flex items-center justify-center text-white text-2xl font-bold mb-4 group relative'
+                className='h-full flex items-center justify-center text-white text-2xl font-bold group relative'
             >
                 <img src={`src/assets/${banner}.jpg`} alt='' className='w-full h-full object-cover rounded-t-xl' />
                 <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-60 transition-opacity duration-200 flex items-center justify-center rounded-t-xl pointer-events-none">
                     <p className="text-white text-3xl font-extrabold">Place Bet</p>
                 </div>
             </Link>
-            <div className='h-35 sm:px-4 pb-4 flex flex-col justify-between' onClick={() => setIsOpen(!isOpen)}>
-                {!isOpen && (
-                    <div className='text-center'>
-                        <h3 className='font-extrabold text-lg md:text-xl text-white mb-2 line-clamp-3'>
-                            <Link
-                                to={`/${bet.id}`}
-                                onClick={(e) => e.stopPropagation()}
-                                className='hover:text-green-400 transition-colors'
-                            >
-                                {bet.bet_text}
-                            </Link>
-                        </h3>
-                    </div>
-                )}
-
-                {isOpen && (
-                    <div className='grid grid-cols-2 gap-2 justify-center'>
+            <div className='h-35 sm:px-4 py-5 flex flex-col justify-between relative overflow-hidden' onClick={() => setIsOpen(!isOpen)}>
+                <div
+                    className={`absolute left-0 top-0 w-full h-full bg-gray-900 z-10 transition-transform duration-300 ease-in-out flex justify-center rounded-b-xl ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+                    <div className='grid grid-cols-2 gap-2 w-full px-2 pt-5'>
                         {bet.choices.map((choice) => (
                             <div key={choice.id} className='text-center mb-2'>
                                 <p className='text-sm text-gray-300 mb-1 font-medium'>
@@ -46,9 +33,13 @@ function Bet({ bet, banner }) {
                             </div>
                         ))}
                     </div>
-                )}
+                </div>
 
-                <div className='text-center mt-2'>
+                <div className={`text-center transition-opacity duration-200 ${isOpen ? 'opacity-0' : 'opacity-100'}`}>
+                    <h3 className='font-extrabold text-lg md:text-xl text-white mb-2 line-clamp-3'>{bet.bet_text}</h3>
+                </div>
+
+                <div className='text-center mt-2 z-50'>
                     <span className='text-xs text-gray-400'>
                         {isOpen ? 'Click to Collapse' : 'Click to See Probabilities'}
                     </span>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -28,11 +28,13 @@ function Home() {
             </div>
             <div className='h-1 bg-gradient-to-l from-gray-900 via-gray-900 to-gray-950'></div>
 
-            <div className='container px-4 md:px-6 lg:px-12 py-8'>
-                <div className='grid grid-cols-2 md:grid-cols-3 gap-6 justify-items-center'>
-                    {bets.map((bet) => (
-                        <Bet key={bet.id} bet={bet} banner={bet.bet_text.toLowerCase().substring(0, bet.bet_text.indexOf(':')).replaceAll(' ', '_')}/>
-                    ))}
+            <div className='flex justify-center'>
+                <div className='container px-4 md:px-6 lg:px-12 py-8'>
+                    <div className='grid grid-cols-2 md:grid-cols-3 gap-6 justify-items-center'>
+                        {bets.map((bet) => (
+                            <Bet key={bet.id} bet={bet} banner={bet.bet_text.toLowerCase().substring(0, bet.bet_text.indexOf(':')).replaceAll(' ', '_')}/>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
